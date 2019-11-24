be2736dbae7f8b5b6f043f4b6b58d3b3d4785885
Atif Afzal <aafzal@sumologic.com>
2019-11-25 02:45:11 +0530

    Render version separately

--- packages/shell/#/utils.ts
+++ packages/shell/#/utils.ts
@@ -9,12 +9,13 @@ export function loadAllModules() {
     .then(a => a.json())
     .then(response => {
       response.docs.forEach((doc: any) => {
+        const target = document.querySelector(doc.targetSelector)!;
         renderModule(
           doc.jsUrl,
           document.querySelector(doc.targetSelector)!,
           doc.moduleName,
-          doc._rev,
         );
+        renderVersion(target, doc._rev);
       });
     });
 }
@@ -34,26 +35,29 @@ export function loadModule(moduleName: string) {
       response.docs.forEach((doc: any) => {
         const target = document.querySelector(doc.targetSelector)!;
 
-        renderModule(doc.jsUrl, target, doc.moduleName, doc._rev);
+        renderModule(doc.jsUrl, target, doc.moduleName);
+        renderVersion(target, doc._rev);
       });
     });
+}
+
+function renderVersion(target: HTMLElement, version: string) {
+  if (
+    target.nextSibling instanceof HTMLElement &&
+    target.nextSibling.getAttribute('data-version')
+  ) {
+    target.nextSibling.innerText = `Rev: ${version.split('-')[0]}`;
+  }
 }
 
 async function renderModule(
   src: string,
   target: HTMLElement,
   moduleName: string,
-  version: string,
 ) {
   try {
     await loadScript(src);
     (window as any).MicroApp[moduleName].render(target);
-    if (
-      target.nextSibling instanceof HTMLElement &&
-      target.nextSibling.getAttribute('data-version')
-    ) {
-      target.nextSibling.innerText = `Rev: ${version.split('-')[0]}`;
-    }
     console.info(`Module: ${moduleName} loaded successfully`);
   } catch (e) {
     console.error(`Could not load module: ${moduleName}, error: `, e);

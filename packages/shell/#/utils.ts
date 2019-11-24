6b75c9414078b24f13bf1084e0f83db0038c672f
Atif Afzal <aafzal@sumologic.com>
2019-11-24 19:39:13 +0530

    Handle script error

--- packages/shell/#/utils.ts
+++ packages/shell/#/utils.ts
@@ -8,16 +8,20 @@ export async function loadModule(
     (window as any).MicroApp[moduleName].render(target);
     console.info(`Module: ${moduleName} loaded successfully`);
   } catch (e) {
-    console.error(`Could not load module: ${moduleName}, error: ${e}`);
+    console.error(`Could not load module: ${moduleName}, error: `, e);
   }
 }
 
 function loadScript(src: string) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const scriptElement = document.createElement('script');
     scriptElement.src = src;
     scriptElement.onload = () => {
       resolve();
+      scriptElement.remove();
+    };
+    scriptElement.onerror = e => {
+      reject(e);
       scriptElement.remove();
     };
     document.body.appendChild(scriptElement);

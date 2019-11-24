d35423880e996ae6406362d0234b25b029a80a81
Atif Afzal <aafzal@sumologic.com>
2019-11-24 15:18:36 +0530

    initial commit with shell

---
+++ webpack.config.ts
@@ -0,0 +1,37 @@
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import * as path from 'path';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: !isProd ? 'development' : 'production',
+
+  devtool: 'source-map',
+
+  entry: path.resolve(__dirname, '#'),
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    modules: ['node_modules', './'],
+  },
+
+  module: {
+    rules: [
+      { test: /\.(ts|js)x?$/, loader: 'ts-loader', exclude: /node_module/ },
+    ],
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: '#/index.html' })],
+
+  devServer: {
+    stats: 'minimal',
+    hot: true,
+    historyApiFallback: true,
+    host: '0.0.0.0',
+  },
+};
